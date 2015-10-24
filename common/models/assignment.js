9317c9f668585d25f94dd92aa603cc5e243bfa09
chrisUsick <christopher.usick@gmail.com>
2015-10-24 12:55:16 -0500

    added averageDuration field on assignment model

--- common/models/assignment.js
+++ common/models/assignment.js
@@ -1,3 +1,25 @@
+var Promise = require('bluebird');
 module.exports = function(Assignment) {
-
+  Assignment.observe('loaded', function(ctx, next) {
+    if (ctx.instance) {
+      var subs = ctx.instance.submissions();
+      if (subs) {
+        ctx.instance.averageDuration = getAverage(subs);
+        next();
+      } else {
+        ctx.instance.submissions(function(err, subs) {
+          ctx.instance.averageDuration = getAverage(subs);
+          next();
+        })
+      }
+    }
+  });
 };
+
+function getAverage(subs) {
+  var total = 0;
+  subs.forEach(function(sub){
+    total += sub.duration;
+  })
+  return total / subs.length;
+}
